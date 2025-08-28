29423e56cfb76940d2006d0277674af3028180c4
sjhawkins24 <serjhawk@iu.edu>
2025-08-28 16:00:07 +0000

    Filling out apputil.py file

--- apputil.py
+++ apputil.py
@@ -1,3 +1,27 @@
 
 
-# add code below ...
+# Exercise 1
+def palindrome(word):
+    '''Function that takes a string and returns true or false if the string is a palindrome'''
+    #Clean up the characters
+    word = word.replace(",", "").replace(".", "").replace(" ", "").replace("?", "").replace("!","").lower()
+    #create new var to hold the reverse of the clean word 
+    rev_word = word[::-1]
+    #Check for equivanence 
+    check = rev_word == word 
+    print(check)
+
+# Exercise 2 
+def parentheses(sequence):
+    '''Function that accepts a string and returns whether the parentheses are balanced. 
+    Also returns which side is unbalanced and by how many '''
+    n_open = sequence.count("(")
+    n_close = sequence.count(")")
+    if n_open == n_close :
+        print("True")
+    elif n_open > n_close :
+        print("False")
+        print(f"{n_open - n_close} more opening parentheses than closing.")
+    else :
+        print("False")
+        print(f"{n_close - n_open} more closing parentheses than opening")
